580bf63530bf80df46505f9f94dea088edd0e8f4
Bryant <bgklug@alaska.edu>
2019-02-17 20:26:47 -0900

    Pass two digit, comma deliminated test

--- stringCal.cpp
+++ stringCal.cpp
@@ -8,8 +8,14 @@
 int stringCal(std::string str){
   if(str.empty()) {
     return 0;
-  } else if(str.length() > 2) {
+  } else if(str.length() == 3) {
     return stoi(str.substr(0,1)) + stoi(str.substr(2,1));
+  } else if(str.length() == 4) {
+    if (str.substr(1,1) == ",") {
+      return stoi(str.substr(0,1)) + stoi(str.substr(2,2));
+    } else {
+      return stoi(str.substr(0,2)) + stoi(str.substr(3,1));
+    }
   } else {
     return stoi(str);
   }
