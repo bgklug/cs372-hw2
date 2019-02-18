d92aac43b992178741ad7dc1b9f53b890ca3e154
Bryant <bgklug@alaska.edu>
2019-02-17 16:57:22 -0900

    Pass comma delimited test

--- stringCal.cpp
+++ stringCal.cpp
@@ -8,6 +8,8 @@
 int stringCal(std::string str){
   if(str.empty()) {
     return 0;
+  } else if(str.length() > 2) {
+    return stoi(str.substr(0,1)) + stoi(str.substr(2,1));
   } else {
     return stoi(str);
   }
